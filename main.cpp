4a79e8ea63f667ccf8ecc5b2a02e078d6f046246
kamilsa <kamilsa16@gmail.com>
2019-04-30 10:13:43 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "math.hpp"
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  std::cout << double_input(2) << std::endl;
+  return 0;
+}
---
+++ math.hpp
@@ -0,0 +1,19 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef RUST_CLIENT__MATH_HPP
+#define RUST_CLIENT__MATH_HPP
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+int double_input(int input);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //RUST_CLIENT__MATH_HPP
